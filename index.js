5c0be7bc889ab972c00141b358909cc0eb33b4b4
Karen Lowe <k.szameit@mac.com>
2017-10-19 13:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = "I don't do much."
+
 kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
-  kittens.push(name)
-  return kittens
+function destructivelyAppendKitten(array, element) {
+  array.push(element)
+  return array
 }
